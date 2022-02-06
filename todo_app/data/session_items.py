185a11219d4e991d4905b1ddc06ea2d96b3710ec
francesco wang <francescowang@outlook.com>
2022-02-06 11:01:59 +0000

    fixed bugs

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,10 +1,11 @@
 from flask import session
 
 _DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    { 'id': 1, 'status': 'Unfinished', 'title': 'Rest' },
+    { 'id': 2, 'status': 'Unfinished', 'title': 'Eat' },
+    { 'id': 3, 'status': 'Unfinished', 'title': 'Code' },
+    { 'id': 4, 'status': 'Unfinished', 'title': 'Gym' },
 ]
-
 
 def get_items():
     """
@@ -16,7 +17,7 @@ def get_items():
     return session.get('items', _DEFAULT_ITEMS.copy())
 
 
-def get_item(id): # remove code and add an API
+def get_item(id):
     """
     Fetches the saved item with the specified ID.
 
@@ -26,11 +27,11 @@ def get_item(id): # remove code and add an API
     Returns:
         item: The saved item, or None if no items match the specified ID.
     """
-    items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
+    tasks = get_items()
+    return next((task for task in tasks if task['id'] == int(id)), None)
 
 
-def add_item(title):  # remove code and add an API
+def add_item(title):
     """
     Adds a new item with the specified title to the session.
 
@@ -40,21 +41,17 @@ def add_item(title):  # remove code and add an API
     Returns:
         item: The saved item.
     """
-    items = get_items()
-
+    tasks = get_items()
     # Determine the ID for the item based on that of the previously added item
-    id = items[-1]['id'] + 1 if items else 0
-
-    item = { 'id': id, 'title': title, 'status': 'Unfinished' }
-
+    id = tasks[-1]['id'] + 1 if tasks else 0
+    task = { 'id': id, 'title': title, 'status': 'Unfinished' }
     # Add the item to the list
-    items.append(item)
-    session['list_of_tasks'] = items
+    tasks.append(task)
+    session['items'] = tasks
+    return task
 
-    return item
 
-
-def save_item(task):  # remove code and add an API
+def save_item(task):
     """
     Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
 
@@ -63,12 +60,10 @@ def save_item(task):  # remove code and add an API
     """
     existing_tasks = get_items()
     updated_tasks = [task if task['id'] == existing_task['id'] else existing_task for existing_task in existing_tasks]
-
     session['list_of_tasks'] = updated_tasks # 'list_of_tasks' can be any name as long as it makes sense
-
     return task
 
-def delete_item(task): # parameter  # remove code and add an API
+def delete_item(task): # parameter # remove code and add an API
     items = get_items()
     items.remove(task) # argument
     session['list_of_tasks'] = items
