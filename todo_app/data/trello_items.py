c7e59bf409f950c4f033afb50b64dcba0fef9b0b
francesco wang <francescowang@outlook.com>
2022-02-24 16:04:55 +0000

    add new functions

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -7,19 +7,19 @@ key = os.environ.get("TRELLO_KEY")
 token = os.environ.get("TOKEN_KEY")
 board = os.environ.get("BOARD_ID")
 not_started_id = os.environ.get("NOT_STARTED_ID")
-completed_id = os.environ.get("COMPLETED_ID")
+completed_id = os.environ.get("COMPLETED_D")
 doing_id = os.environ.get("DOING_ID")
 
 
 class Task:
-    def __init__(self, id, name, status = "Not Started"):
-        self.id = id
+    def __init__(self, item_id, name, status = "Not Started"):
+        self.item_id = item_id
         self.name = name
         self.status = status
         
     @classmethod
     def from_trello_card(cls, card, list_name):
-        return cls(card["id"], card["name"], list_name)
+        return cls(card["item_id"], card["name"], list_name)
 
 
 def get_cards(list_name):
@@ -48,7 +48,57 @@ def get_cards(list_name):
 
 
 def add_trello_task(title):
-    pass
+    url_api = f"https://api.trello.com/1/lists/{not_started_id}/cards?name={title}&key={key}&token={token}"
 
-card = get_cards("Not Started")
-print(card)
+    headers = {
+    "Accept": "application/json"
+    }
+
+    response = requests.request(
+    "POST",
+    url=url_api,
+    headers=headers
+    )
+    
+def delete_trello_task(item_id):
+
+    call = f"https://api.trello.com/1/cards/{item_id}?key={key}&token={token}"
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    return requests.request("DELETE", url=call, headers=headers)
+
+
+def completed_trello_task(item_id):
+
+    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={completed_id}&key={key}&token={token}"
+
+    headers = {
+    "Accept": "application/json"
+    }
+    
+    return requests.request("PUT", url=url_api, headers=headers)
+
+
+def doing_trello_task(item_id):
+
+    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={doing_id}&key={key}&token={token}"
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    return requests.request("PUT", url=url_api, headers=headers)
+
+
+def unfinished_trello_task(item_id):
+
+    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={doing_id}&key={key}&token={token}"
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    return requests.request("PUT", url=url_api, headers=headers)
