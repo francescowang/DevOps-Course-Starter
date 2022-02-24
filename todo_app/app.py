3a9efb648b5adea4a9166774930095d61c3732fd
francesco wang <francescowang@outlook.com>
2022-02-24 18:40:01 +0000

    reformatted code - yet to fix bugs

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,49 +1,59 @@
 from flask import Flask, request, render_template, redirect, url_for
-from todo_app.data.session_items import get_items, get_item, add_item, delete_item, save_item
+from todo_app.data.trello_items import *
 from todo_app.flask_config import Config
-import todo_app.data.trello_items 
 
 
 app = Flask(__name__)
-# app.secret_key="WHY!"
 app.config.from_object(Config())
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    items = todo_app.data.trello_items.get_cards("Not Started")
-    items.sort(key = lambda task: task.status, reverse=True) # status refers to a key in session_items on line 4
-    return render_template('index.html', items_jinja=items) # items_jinja refers to index.html on line 15
+    not_started_tasks = get_cards("not started")
+    doing_tasks = get_cards("doing")
+    completed_tasks = get_cards("completed")
+    return render_template(
+        "index.html",
+        not_started_tasks=not_started_tasks,
+        doing_tasks=doing_tasks,
+        completed_tasks=completed_tasks,
+    )
 
 
-@app.route('/add_task', methods=['POST'])
+# Add
+@app.route("/add_task", methods=["POST"])
 def add_task():
-    add_item(title=request.form.get('item_name'))
-    return redirect(url_for('index'))
+    add_trello_task(title=request.form.get("item_name"))
+    return redirect(url_for("index"))
 
 
-@app.route('/delete/<item_id>', methods=['POST'])
-def delete_task(item_id):
-    item = get_item(item_id)
-    delete_item(task=item)
-    return redirect(url_for('index'))    
+# Delete
+@app.route("/delete/<id>", methods=["POST"])
+def delete_task(id):
+    delete_trello_task(id=request.form["delete_id"])
+    return redirect(url_for("index"))
 
 
-@app.route('/completed/<item_id>', methods=['POST'])
-def mark_as_complete(item_id):
-    item = get_item(item_id)
-    item["status"] = "Completed"
-    save_item(task=item) # key -> that's the parameter of the function save_item(), line 62
-    return redirect(url_for('index'))
+# Not Started
+@app.route("/not_started_task/<id>", methods=["POST"])
+def not_started_task(id):
+    not_started_trello_task(id=request.form["not_started_id"])
+    return redirect(url_for("index"))
 
 
-@app.route('/not_started/<item_id>', methods=['POST'])
-def mark_as_to_do(item_id):
-    item = get_item(item_id)
-    item["status"] = "Not Started"
-    save_item(task=item)
-    return redirect(url_for('index'))
+# Doing
+@app.route("/doing_task/<id>", methods=["POST"])
+def doing_task(id):
+    doing_trello_task(id=request.form["doing_id"])
+    return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
+# Completed
+@app.route("/completed/<id>", methods=["POST"])
+def completed_task(id):
+    completed_trello_task(id=request.form["completed_id"])
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
     app.run()
