f01ac13e0a05ca45f71667cb885fbc08321c5a53
francesco wang <francescowang@outlook.com>
2022-02-24 18:39:16 +0000

    ran black trello_items.py

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -7,98 +7,84 @@ key = os.environ.get("TRELLO_KEY")
 token = os.environ.get("TOKEN_KEY")
 board = os.environ.get("BOARD_ID")
 not_started_id = os.environ.get("NOT_STARTED_ID")
-completed_id = os.environ.get("COMPLETED_D")
 doing_id = os.environ.get("DOING_ID")
+completed_id = os.environ.get("COMPLETED_ID")
 
 
 class Task:
-    def __init__(self, item_id, name, status = "Not Started"):
-        self.item_id = item_id
+    def __init__(self, id, name, status="Not Started"):
+        self.id = id
         self.name = name
         self.status = status
-        
+
     @classmethod
     def from_trello_card(cls, card, list_name):
-        return cls(card["item_id"], card["name"], list_name)
+        return cls(card["id"], card["name"], list_name)
 
 
 def get_cards(list_name):
     url_api = f"https://api.trello.com/1/boards/{board}/lists?key={key}&token={token}&cards=open"
-    headers = {
-        "Accept": "application/json"
-    }
-    
-    response = requests.request(
-        "GET",
-        url=url_api,
-        headers=headers
-    )
-    
+    headers = {"Accept": "application/json"}
+
+    response = requests.request("GET", url=url_api, headers=headers)
+
     result = response.json()
-    
+
     tasks = []
-    
-    for a_list in result: # Not Started, Completed, Doing
+
+    for a_list in result:  # a_lists refer to Not Started, Doing, Completed lists
         if a_list["name"] == list_name:
             for card in a_list["cards"]:
                 task = Task.from_trello_card(card, list_name)
-                
+
                 tasks.append(task)
     return tasks
 
 
+# Add
 def add_trello_task(title):
     url_api = f"https://api.trello.com/1/lists/{not_started_id}/cards?name={title}&key={key}&token={token}"
 
-    headers = {
-    "Accept": "application/json"
-    }
+    headers = {"Accept": "application/json"}
 
-    response = requests.request(
-    "POST",
-    url=url_api,
-    headers=headers
-    )
-    
-def delete_trello_task(item_id):
-
-    call = f"https://api.trello.com/1/cards/{item_id}?key={key}&token={token}"
-
-    headers = {
-    "Accept": "application/json"
-    }
-
-    return requests.request("DELETE", url=call, headers=headers)
+    response = requests.request("POST", url=url_api, headers=headers)
 
 
-def completed_trello_task(item_id):
+# Delete
+def delete_trello_task(id):
 
-    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={completed_id}&key={key}&token={token}"
+    url_api = f"https://api.trello.com/1/cards/{id}?key={key}&token={token}"
 
-    headers = {
-    "Accept": "application/json"
-    }
-    
-    return requests.request("PUT", url=url_api, headers=headers)
+    headers = {"Accept": "application/json"}
+
+    return requests.request("DELETE", url=url_api, headers=headers)
 
 
-def doing_trello_task(item_id):
+# Not Started
+def not_started_trello_task(id):
 
-    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={doing_id}&key={key}&token={token}"
+    url_api = f"https://api.trello.com/1/cards/{id}?item_idList={doing_id}&key={key}&token={token}"
 
-    headers = {
-    "Accept": "application/json"
-    }
+    headers = {"Accept": "application/json"}
 
     return requests.request("PUT", url=url_api, headers=headers)
 
 
-def unfinished_trello_task(item_id):
+# Doing
+def doing_trello_task(id):
 
-    url_api = f"https://api.trello.com/1/cards/{item_id}?item_idList={doing_id}&key={key}&token={token}"
+    url_api = f"https://api.trello.com/1/cards/{id}?item_idList={doing_id}&key={key}&token={token}"
 
-    headers = {
-    "Accept": "application/json"
-    }
+    headers = {"Accept": "application/json"}
+
+    return requests.request("PUT", url=url_api, headers=headers)
+
+
+# Completed
+def completed_trello_task(id):
+
+    url_api = f"https://api.trello.com/1/cards/{id}?item_idList={completed_id}&key={key}&token={token}"
+
+    headers = {"Accept": "application/json"}
 
     return requests.request("PUT", url=url_api, headers=headers)
