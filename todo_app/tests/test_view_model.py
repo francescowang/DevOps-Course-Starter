e0361735d5c772763e77960f0a6705198c33d6be
francesco wang <francescowang@outlook.com>
2022-03-10 00:11:45 +0000

    test view_model.py

---
+++ todo_app/tests/test_view_model.py
@@ -0,0 +1,43 @@
+from todo_app.view_model import TaskViewModel
+from todo_app.data.trello_items import TaskStatus
+
+
+def test_not_started_tasks():
+    # Arrange
+    items = [
+        TaskStatus("Task 1", "Code", "Not Started"),
+        TaskStatus("Task 2", "Sleep", "Doing"),
+        TaskStatus("Task 3", "Gym", "Completed")            
+    ]
+
+    view_model = TaskViewModel(items)
+    
+    # Act
+    not_started_tasks = view_model.not_started_items
+
+    # Assert
+    assert len(view_model.not_started_items) == 1
+
+
+def test_doing_tasks():
+    items = [
+        TaskStatus("Task 1", "Code", "Not Started"),
+        TaskStatus("Task 2", "Sleep", "Doing"),
+        TaskStatus("Task 3", "Gym", "Completed")  
+    ]
+    
+    view_model = TaskViewModel(items)
+
+    assert len(view_model.doing_items) == 1
+
+
+def test_completed_tasks():
+    items = [
+        TaskStatus("Task 1", "Code", "Not Started"),
+        TaskStatus("Task 2", "Sleep", "Doing"),
+        TaskStatus("Task 3", "Gym", "Completed")  
+    ]
+    
+    view_model = TaskViewModel(items)
+
+    assert len(view_model.completed_items) == 1
