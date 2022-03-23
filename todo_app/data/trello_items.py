5de102331d94046b12dad17afaf9a0d0768b60e6
francesco wang <francescowang@outlook.com>
2022-03-23 09:21:11 +0000

    fix bugs

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,16 +1,6 @@
 import os
 import requests
 
-
-# global variables
-key = os.environ.get("TRELLO_KEY")
-token = os.environ.get("TOKEN_KEY")
-board = os.environ.get("BOARD_ID")
-not_started_id = os.environ.get("NOT_STARTED_ID")
-doing_id = os.environ.get("DOING_ID")
-completed_id = os.environ.get("COMPLETED_ID")
-
-
 class TaskStatus:
     # does the parameter status also refer to "Doing" and "Completed"?
     def __init__(self, id, name, status): # status="Not Started"
@@ -23,68 +13,77 @@ class TaskStatus:
         return cls(card["id"], card["name"], list_name)
 
 
-def get_cards():
-    url_api = f"https://api.trello.com/1/boards/{board}/lists?key={key}&token={token}&cards=open"
-    headers = {"Accept": "application/json"}
-
-    response = requests.request("GET", url=url_api, headers=headers)
-
-    result = response.json()
-
-    tasks = []
-
-    for a_list in result:  # a_lists refer to NOT STARTED, DOING, COMPLETED lists in trello board
-        for card in a_list["cards"]:
-            task = TaskStatus.from_trello_card(card, a_list["name"]) # calling all lists -> a_list["name"]
-            tasks.append(task)
-    return tasks
+class TrelloItems:
+    def __init__(self):
+        self.key = os.environ.get("TRELLO_KEY")
+        self.token = os.environ.get("TOKEN_KEY")
+        self.board = os.environ.get("BOARD_ID")
+        self.not_started_id = os.environ.get("NOT_STARTED_ID")
+        self.doing_id = os.environ.get("DOING_ID")
+        self.completed_id = os.environ.get("COMPLETED_ID")
 
 
-# Add
-def add_trello_task(title):
-    url_api = f"https://api.trello.com/1/lists/{not_started_id}/cards?name={title}&key={key}&token={token}"
+    def get_cards(self):
+        url_api = f"https://api.trello.com/1/boards/{self.board}/lists?key={self.key}&token={self.token}&cards=open"
+        headers = {"Accept": "application/json"}
 
-    headers = {"Accept": "application/json"}
+        response = requests.request("GET", url=url_api, headers=headers)
 
-    response = requests.request("POST", url=url_api, headers=headers)
+        result = response.json()
 
+        tasks = []
 
-# Delete
-def delete_trello_task(delete_task):
+        for a_list in result:  # a_lists refer to NOT STARTED, DOING, COMPLETED lists in trello board
+            for card in a_list["cards"]:
+                task = TaskStatus.from_trello_card(card, a_list["name"]) # calling all lists -> a_list["name"]
+                tasks.append(task)
+        return tasks
 
-    url_api = f"https://api.trello.com/1/cards/{delete_task}?key={key}&token={token}"
+    # Add
+    def add_trello_task(self, title):
+        url_api = f"https://api.trello.com/1/lists/{self.not_started_id}/cards?name={title}&key={self.key}&token={self.token}"
 
-    headers = {"Accept": "application/json"}
+        headers = {"Accept": "application/json"}
 
-    return requests.request("DELETE", url=url_api, headers=headers)
-
-
-# Not Started
-def not_started_trello_task(not_started_task):
-
-    url_api = f"https://api.trello.com/1/cards/{not_started_task}?idList={not_started_id}&key={key}&token={token}"
-
-    headers = {"Accept": "application/json"}
-
-    return requests.request("PUT", url=url_api, headers=headers)
+        response = requests.request("POST", url=url_api, headers=headers)
 
 
-# Doing
-def doing_trello_task(doing_task):
+    # Delete
+    def delete_trello_task(self, delete_task):
 
-    url_api = f"https://api.trello.com/1/cards/{doing_task}?idList={doing_id}&key={key}&token={token}"
-    print(url_api)
+        url_api = f"https://api.trello.com/1/cards/{delete_task}?key={self.key}&token={self.token}"
 
-    headers = {"Accept": "application/json"}
+        headers = {"Accept": "application/json"}
 
-    return requests.request("PUT", url=url_api, headers=headers)
+        return requests.request("DELETE", url=url_api, headers=headers)
 
 
-# Completed
-def completed_trello_task(completed_task):
+    # Not Started
+    def not_started_trello_task(self, not_started_task):
 
-    url_api = f"https://api.trello.com/1/cards/{completed_task}?idList={completed_id}&key={key}&token={token}"
+        url_api = f"https://api.trello.com/1/cards/{not_started_task}?idList={self.not_started_id}&key={self.key}&token={self.token}"
 
-    headers = {"Accept": "application/json"}
+        headers = {"Accept": "application/json"}
 
-    return requests.request("PUT", url=url_api, headers=headers)
+        return requests.request("PUT", url=url_api, headers=headers)
+
+
+    # Doing
+    def doing_trello_task(self, doing_task):
+
+        url_api = f"https://api.trello.com/1/cards/{doing_task}?idList={self.doing_id}&key={self.key}&token={self.token}"
+        print(url_api)
+
+        headers = {"Accept": "application/json"}
+
+        return requests.request("PUT", url=url_api, headers=headers)
+
+
+    # Completed
+    def completed_trello_task(self, completed_task):
+
+        url_api = f"https://api.trello.com/1/cards/{completed_task}?idList={self.completed_id}&key={self.key}&token={self.token}"
+
+        headers = {"Accept": "application/json"}
+
+        return requests.request("PUT", url=url_api, headers=headers)
