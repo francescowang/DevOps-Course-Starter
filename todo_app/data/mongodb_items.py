6be1401574dc23a6550952a8493c93d22e615184
francesco wang <francescowang@outlook.com>
2022-09-20 16:33:18 +0100

    Add mongodb

---
+++ todo_app/data/mongodb_items.py
@@ -0,0 +1,74 @@
+import os
+import pymongo
+from todo_app.data.trello_items import TaskStatus
+from bson import ObjectId
+
+
+
+class MongoDB_Items:
+    def __init__(self):
+        connection_string = os.environ.get("CONNECTION_STRING")
+        mongodb = os.environ.get("MONGO_DB")
+        client = pymongo.MongoClient(connection_string)
+        db = client[mongodb]
+        
+        self.collection = db.todo_collection
+
+
+
+    def get_mongo_cards(self):
+        return [TaskStatus.from_mongodb_card(card) for card in self.collection.find()]
+    
+    # title of the task
+    def add_mongo_card(self, title):
+        self.collection.insert_one({
+            "name": title,
+            "status": "NOT STARTED"
+        })
+    
+    # id of the task
+    def delete_mongo_card(self, id):
+        # self.collection.delete_one(
+
+        # )
+        # items = { "address": "Mountain 21" }
+        queries = { 
+                "_id": ObjectId(id)
+        }
+        print(type(queries))
+        self.collection.delete_one(queries)
+
+    # id refers to the id of the task
+    # func updates the status of the task
+    def not_started_mongo_card(self, id):
+        
+        query = {
+            "_id": ObjectId(id)
+        }
+        new_query = {
+            "$set": {"status": "NOT STARTED"}
+            
+        }
+        self.collection.update_one(query, new_query)
+    
+    def doing_mongo_card(self, id):
+        
+        query = {
+            "_id": ObjectId(id)
+        }
+        new_query = {
+            "$set": {"status": "DOING"}
+            
+        }
+        self.collection.update_one(query, new_query)
+
+    def completed_mongo_card(self, id):
+        
+        query = {
+            "_id": ObjectId(id)
+        }
+        new_query = {
+            "$set": {"status": "COMPLETED"}
+            
+        }
+        self.collection.update_one(query, new_query)
