1e722cddae785f62aa4f3bac0a69347ce7255758
francesco wang <francescowang@outlook.com>
2023-02-01 12:18:23 +0000

    Implement the callback route

---
+++ todo_app/users.py
@@ -0,0 +1,17 @@
+import os
+from flask_login import UserMixin
+
+
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def get_id(self):
+        return self.user_id
+    
+    @property
+    def role(self):
+        if self.user_id == int(os.environ.get("ADMIN_ID")):
+            return "admin"
+        else:
+            return "reader"
